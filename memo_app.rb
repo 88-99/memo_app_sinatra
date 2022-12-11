ad45dcc6365628da6db6651483aad980174904ea
88-99 <xz835.525o39@yahoo.com>
2022-12-11 19:23:05 +0900

    sinatra/namespaceを導入

--- memo_app.rb
+++ memo_app.rb
@@ -4,63 +4,66 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'securerandom' # uuid
+require 'sinatra/namespace'
 
 set :environment, :development
 
-get '/memos' do
-  @memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+namespace '/memos' do
+  get '' do
+    @memos = File.open('memos.json') { |f| JSON.parse(f.read) }
 
-  erb :index
-end
+    erb :index
+  end
 
-get '/memos/new' do
-  erb :new
-end
+  get '/new' do
+    erb :new
+  end
 
-post '/memos/posts' do
-  title = escape_html(params[:title]).to_s
-  content = escape_html(params[:content]).to_s
-  id = SecureRandom.uuid
+  post '/posts' do
+    title = escape_html(params[:title]).to_s
+    content = escape_html(params[:content]).to_s
+    id = SecureRandom.uuid
 
-  memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-  memos['memos'] << { 'id' => id, 'title' => title, 'content' => content }
-  File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    memos['memos'] << { 'id' => id, 'title' => title, 'content' => content }
+    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
 
-  redirect '/memos'
-end
+    redirect '/memos'
+  end
 
-get '/memos/:id' do
-  memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-  @memo = memos['memos'].find { |memo| memo['id'] == escape_html(params[:id]).to_s }
+  get '/:id' do
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    @memo = memos['memos'].find { |memo| memo['id'] == escape_html(params[:id]).to_s }
 
-  erb :show
-end
+    erb :show
+  end
 
-get '/memos/:id/edit' do
-  memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-  @memo = memos['memos'].find { |memo| memo['id'] == escape_html(params[:id]).to_s }
+  get '/:id/edit' do
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    @memo = memos['memos'].find { |memo| memo['id'] == escape_html(params[:id]).to_s }
 
-  erb :edit
-end
+    erb :edit
+  end
 
-patch '/memos/edit' do
-  title = escape_html(params[:title]).to_s
-  content = escape_html(params[:content]).to_s
-  id = escape_html(params[:id]).to_s
+  patch '/edit' do
+    title = escape_html(params[:title]).to_s
+    content = escape_html(params[:content]).to_s
+    id = escape_html(params[:id]).to_s
 
-  memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-  edited_data = { 'id' => id, 'title' => title, 'content' => content }
-  memos['memos'].map! { |memo| memo['id'] == id ? edited_data : memo }
-  File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    edited_data = { 'id' => id, 'title' => title, 'content' => content }
+    memos['memos'].map! { |memo| memo['id'] == id ? edited_data : memo }
+    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
 
-  redirect '/memos'
-end
+    redirect '/memos'
+  end
 
-delete '/memos/del' do
-  memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-  memo_index = memos['memos'].index { |memo| memo['id'] == escape_html(params[:id]).to_s }
-  memos['memos'].delete_at(memo_index) # .destroyができなかったのでrubyでmemos[]から削除。
-  File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
+  delete '/del' do
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    memo_index = memos['memos'].index { |memo| memo['id'] == escape_html(params[:id]).to_s }
+    memos['memos'].delete_at(memo_index) # .destroyができなかったのでrubyでmemos[]から削除。
+    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
 
-  redirect '/memos'
+    redirect '/memos'
+  end
 end
