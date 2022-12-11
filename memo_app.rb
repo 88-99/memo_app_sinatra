05c0c47564273f6cefd6fc4fbe0217116540ce35
88-99 <xz835.525o39@yahoo.com>
2022-12-11 13:19:38 +0900

    新規画面と詳細画面を作成

--- memo_app.rb
+++ memo_app.rb
@@ -1,7 +1,33 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+require 'securerandom' # uuid
 
-set :environment, :production
+set :environment, :development
 
-get '/' do
+get '/memos' do
   erb :index
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+post '/memos/posts' do
+  title = "#{escape_html(params[:title])}"
+  content = "#{escape_html(params[:content])}"
+  id = SecureRandom.uuid
+
+  memos = File.open("memos.json") { |f| JSON.load(f) }
+  memos["memos"] << { "id" => id, "title" => title, "content" => content }
+  File.open("memos.json", "w") { |f| JSON.dump(memos, f) }
+
+  redirect '/memos'
+end
+
+get '/memos/:id' do
+  memos = File.open("memos.json") { |f| JSON.load(f) }
+  @memo = memos["memos"].find {|memo| memo["id"] == "#{params[:id]}" }
+
+  erb :show
 end
