35ed2b3e3668fa770248425ad9fe4c8058206af1
88-99 <xz835.525o39@yahoo.com>
2022-12-15 08:00:01 +0900

    helpersにモジュールとしてメソッド抽出

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -1,7 +1,25 @@
-# frozen_string_literal: true
-
 module Helper
   def escape_html
     Rack::Utils.escape_html(text)
+  end
+
+  def recieve_params(title, content, id)
+    title = escape_html(title).to_s
+    content = escape_html(content).to_s
+    id = id.nil? ? SecureRandom.uuid : escape_html(id).to_s
+    [title, content, id]
+  end
+
+  def validate(title, content, id)
+    if [title, content].any?(&:empty?) || title.length > 30 || content.length > 1000
+      # (title || content).empty? では、(title = "123") && (content = "")でfalseになり機能しない。
+      redirect "/memos/#{id}/edit"
+    else
+      redirect '/memos'
+    end
+  end
+
+  def json_dump(memos)
+    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
   end
 end
--- memo_app.rb
+++ memo_app.rb
@@ -5,14 +5,14 @@ require 'sinatra/reloader'
 require 'json'
 require 'securerandom' # uuid
 require 'sinatra/namespace'
+require_relative 'helpers/helpers.rb'
 require 'byebug'
 
 set :environment, :development
 
-namespace '/memos' do
+namespace '/memos' do # RuBocopより、 Block has too many lines. [39/25] namespace '/memos' do ... と指摘されており未解決。
   get '' do
     @memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-
     erb :index
   end
 
@@ -21,60 +21,41 @@ namespace '/memos' do
   end
 
   post '/posts' do
-    title = escape_html(params[:title]).to_s
-    content = escape_html(params[:content]).to_s
-    id = SecureRandom.uuid
-
+    title, content, id = recieve_params(params[:title], params[:content], params[:id])
     memos = File.open('memos.json') { |f| JSON.parse(f.read) } # 重複を避けるために before do でまとめた場合、インスタンス変数(@memos)は増える。それが良いのか不明。
     memos['memos'] << { 'id' => id, 'title' => title, 'content' => content }
-    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
-
-    if [title, content].any?(&:empty?) || title.length > 30 || content.length > 1000
-      # (title || content).empty? では、(title = "123") && (content = "")でfalseになり機能しない。
-      error_id = memos['memos'].last['id']
-      redirect "/memos/#{error_id}/edit"
-    else
-      redirect '/memos'
-    end
+    json_dump(memos)
+    validate(title, content, id) # 入力情報を検証しているが、
+    # 一度入力した情報を消さないままエラーメッセージを表示するロジックを考えられていないため、一度保存する流れになっている。検証した意味が今のところない。
+    # 保存してバリデーションに引っかかると/memos/:id/editに遷移する。
   end
 
   get '/:id' do
     memos = File.open('memos.json') { |f| JSON.parse(f.read) }
     @memo = memos['memos'].find { |memo| memo['id'] == escape_html(params[:id]).to_s }
-
     erb :show
   end
 
   get '/:id/edit' do
     memos = File.open('memos.json') { |f| JSON.parse(f.read) }
     @memo = memos['memos'].find { |memo| memo['id'] == escape_html(params[:id]).to_s }
-
     erb :edit
   end
 
   patch '/edit' do
-    title = escape_html(params[:title]).to_s
-    content = escape_html(params[:content]).to_s
-    id = escape_html(params[:id]).to_s
-
+    title, content, id = recieve_params(params[:title], params[:content], params[:id])
     memos = File.open('memos.json') { |f| JSON.parse(f.read) }
     edited_data = { 'id' => id, 'title' => title, 'content' => content }
     memos['memos'].map! { |memo| memo['id'] == id ? edited_data : memo }
-    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
-
-    if [title, content].any?(&:empty?) || title.length > 30 || content.length > 1000
-      redirect "/memos/#{id}/edit"
-    else
-      redirect '/memos'
-    end
+    json_dump(memos)
+    validate(title, content, id) # 保存してバリデーションに引っかかると/memos/:id/editに遷移する。
   end
 
   delete '/del' do
     memos = File.open('memos.json') { |f| JSON.parse(f.read) }
     memo_index = memos['memos'].index { |memo| memo['id'] == escape_html(params[:id]).to_s }
     memos['memos'].delete_at(memo_index) # .destroyができなかったのでrubyでmemos[]から削除。
-    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
-
+    json_dump(memos)
     redirect '/memos'
   end
 end
