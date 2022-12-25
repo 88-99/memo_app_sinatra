41845b604f7deffc38fc414d120ac2d59ee1e4a8
88-99 <xz835.525o39@yahoo.com>
2022-12-25 23:12:21 +0900

    refactoring zero

--- memo_app.rb
+++ memo_app.rb
@@ -22,27 +22,38 @@ namespace '/memos' do
   end
 
   post '' do
-    Post.post_contents(params[:title], params[:content])
+    id = SecureRandom.uuid
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    memos['memos'] << { id:, title: params[:title], content: params[:content] }
+    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
     redirect '/memos'
   end
 
   get '/:id' do
-    @memo = Get.get_contents(params[:id])
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    @memo = memos['memos'].find { |memo| memo['id'] == params[:id] }
     erb :show
   end
 
   get '/:id/edit' do
-    @memo = Get.get_contents(params[:id])
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    @memo = memos['memos'].find { |memo| memo['id'] == params[:id] }
     erb :edit
   end
 
   patch '' do
-    Patch.patch_contents(params[:id], params[:title], params[:content])
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    edited_data = { id: params[:id], title: params[:title], content: params[:content] }
+    @memo = memos['memos'].map! { |memo| memo['id'] == params[:id] ? edited_data : memo }
+    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
     redirect '/memos'
   end
 
   delete '/:id' do
-    Delete.delete_contents(params[:id])
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    memo_index = memos['memos'].index { |memo| memo['id'] == params[:id] }
+    memos['memos'].delete_at(memo_index) # .destroyができなかったのでrubyでmemos[]から削除。
+    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
     redirect '/memos'
   end
 end
