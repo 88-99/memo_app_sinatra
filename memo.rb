6323856c16bc5e3c098947fa25c181f81b5402ec
88-99 <xz835.525o39@yahoo.com>
2022-12-28 08:07:46 +0900

    メモクラスに json_to_rubyメソッドを定義

--- memo.rb
+++ memo.rb
@@ -3,8 +3,19 @@ class Memo
   def initialize(title, content)
     @title = title
     @content = content
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
   end
 
-end
+  def json_to_ruby
+    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    memos['memos']
+  end
 
+  # def write_file(memos)
+  #   File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
+  # end
+
+  # def show_memo
+  #   memos = json_to_ruby
+  #   @memo = memos['memos'].find { |memo| memo['id'] == params[:id] }
+  # end
+end
--- memo_app.rb
+++ memo_app.rb
@@ -11,9 +11,23 @@ require 'debug'
 
 set :environment, :development
 
+# def json_to_ruby
+#   File.open('memos.json') { |f| JSON.parse(f.read) }
+# end
+
+def write_file(memos)
+  File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
+end
+
+def show_memo
+  memos = json_to_ruby
+  @memo = memos['memos'].find { |memo| memo['id'] == params[:id] }
+end
+
 namespace '/memos' do
   get '' do
     @memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    # @memos = json_to_ruby
     erb :index
   end
 
@@ -23,38 +37,36 @@ namespace '/memos' do
 
   post '' do
     id = SecureRandom.uuid
-    Memo.new(params[:title], params[:content])
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-    memos['memos'] << { id:, title: params[:title], content: params[:content] }
-    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
+    memo = Memo.new(params[:title], params[:content])
+    memo.json_to_ruby << { id:, title: memo.title, content: memo.content }
+    # memos['memos'] << { id:, title: memo.title, content: memo.content }
+    write_file(memos)
     redirect '/memos'
   end
 
   get '/:id' do
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-    @memo = memos['memos'].find { |memo| memo['id'] == params[:id] }
+    show_memo
     erb :show
   end
 
   get '/:id/edit' do
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-    @memo = memos['memos'].find { |memo| memo['id'] == params[:id] }
+    show_memo
     erb :edit
   end
 
   patch '' do
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    memos = json_to_ruby
     edited_data = { id: params[:id], title: params[:title], content: params[:content] }
     @memo = memos['memos'].map! { |memo| memo['id'] == params[:id] ? edited_data : memo }
-    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
+    write_file(memos)
     redirect '/memos'
   end
 
   delete '/:id' do
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
+    memos = json_to_ruby
     memo_index = memos['memos'].index { |memo| memo['id'] == params[:id] }
     memos['memos'].delete_at(memo_index) # .destroyができなかったのでrubyでmemos[]から削除。
-    File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
+    write_file(memos)
     redirect '/memos'
   end
 end
