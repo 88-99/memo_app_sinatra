84806e793c5249dd40fda32fa3ca92f173155404
88-99 <xz835.525o39@yahoo.com>
2022-12-29 22:36:16 +0900

    リファクタリング

--- memo.rb
+++ memo.rb
@@ -1,27 +1,54 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'securerandom' # uuid
+
 class Memo
-  attr_accessor :title, :content
-  def initialize(title, content)
+  attr_accessor :id, :title, :content
+
+  def initialize(id, title, content)
+    @id = id.nil? ? SecureRandom.uuid : id
     @title = title
     @content = content
   end
 
-  def post_contents
-    id = SecureRandom.uuid
-    memos = json_to_ruby
-    memos['memos'] << { id:, title: @title, content: @content }
+  def self.index
+    memos = Memo.json_to_ruby
+    memos['memos'].map { |memo| Memo.new(memo['id'], memo['title'], memo['content']) }
+  end
+
+  def create
+    memos = Memo.json_to_ruby
+    memos['memos'] << { id: @id, title: @title, content: @content }
     write_file(memos)
   end
 
-  def json_to_ruby
-    File.open('memos.json') { |f| JSON.parse(f.read) }
+  def update
+    memos = Memo.json_to_ruby
+    edited_data = { id: @id, title: @title, content: @content }
+    memos['memos'].map! { |memo| memo['id'] == @id ? edited_data : memo }
+    write_file(memos)
+  end
+
+  def delete
+    memos = Memo.json_to_ruby
+    memo_index = memos['memos'].index { |memo| memo['id'] == @id }
+    memos['memos'].delete_at(memo_index)
+    write_file(memos)
+  end
+
+  def self.json_to_ruby
+    File.open('memos.json') { |f| JSON.parse(f.read) } # JSON.parse の第2引数にオプション symbolize_names: true を指定すると、ハッシュのキーを文字列ではなくシンボルにできる。
   end
 
   def write_file(memos)
     File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
   end
 
-  # def show_memo
-  #   memos = json_to_ruby
-  #   @memo = memos['memos'].find { |memo| memo['id'] == params[:id] }
-  # end
+  def self.show_memo(id)
+    # 特異=スタティック=クラスメソッドはインスタンスメソッドを呼べない。同じ特異メソッドは呼べる。
+    memos = Memo.json_to_ruby
+    memo = memos['memos'].find { |m| m['id'] == id } # == @idを書いても違う領域の@idを見てしまうので@idはnill。そのため引数でparams[:id]を渡す。
+    Memo.new(memo['id'], memo['title'], memo['content'])
+  end
 end
--- memo_app.rb
+++ memo_app.rb
@@ -2,19 +2,14 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
-require 'securerandom' # uuid
 require 'sinatra/namespace'
 require_relative 'memo'
-require 'byebug'
-require 'debug'
 
 set :environment, :development
 
 namespace '/memos' do
   get '' do
-    @memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-    # @memos = json_to_ruby
+    @memos = Memo.index
     erb :index
   end
 
@@ -23,36 +18,28 @@ namespace '/memos' do
   end
 
   post '' do
-    memo = Memo.new(params[:title], params[:content])
-    memo.post_contents
+    Memo.new(nil, params[:title], params[:content]).create
     redirect '/memos'
   end
 
   get '/:id' do
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-    @memo = memos['memos'].find { |memo| memo['id'] == params[:id] }
+    @memo = Memo.show_memo(params[:id])
     erb :show
   end
 
   get '/:id/edit' do
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-    @memo = memos['memos'].find { |memo| memo['id'] == params[:id] }
+    @memo = Memo.show_memo(params[:id])
     erb :edit
   end
 
   patch '' do
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-    edited_data = { id: params[:id], title: params[:title], content: params[:content] }
-    @memo = memos['memos'].map! { |memo| memo['id'] == params[:id] ? edited_data : memo }
-    write_file(memos)
+    @memo = Memo.new(params[:id], params[:title], params[:content])
+    @memo.update
     redirect '/memos'
   end
 
   delete '/:id' do
-    memos = File.open('memos.json') { |f| JSON.parse(f.read) }
-    memo_index = memos['memos'].index { |memo| memo['id'] == params[:id] }
-    memos['memos'].delete_at(memo_index) # .destroyができなかったのでrubyでmemos[]から削除。
-    write_file(memos)
+    Memo.show_memo(params[:id]).delete
     redirect '/memos'
   end
 end
