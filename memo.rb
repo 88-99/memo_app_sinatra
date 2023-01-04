8e97ebc3b7d4fcb07f0710e6ac51430f2137a01d
88-99 <xz835.525o39@yahoo.com>
2023-01-04 14:12:09 +0900

    PG.connect(dbname: 'memo')をメソッドに抽出

--- memo.rb
+++ memo.rb
@@ -13,30 +13,25 @@ class Memo
     @content = content
   end
 
-  def self.index
-    connection = PG.connect(dbname: 'memo')
-    memos = connection.exec('SELECT * FROM memos')
+  def self.index(conn)
+    memos = conn.exec('SELECT * FROM memos')
     memos.map { |memo| Memo.new(memo['id'], memo['title'], memo['content']) }
   end
 
-  def create
-    connection = PG.connect(dbname: 'memo')
-    connection.exec('INSERT INTO memos VALUES ($1, $2, $3)', [@id, @title, @content])
+  def create(conn)
+    conn.exec('INSERT INTO memos VALUES ($1, $2, $3)', [@id, @title, @content])
   end
 
-  def update
-    connection = PG.connect(dbname: 'memo')
-    connection.exec("UPDATE memos SET title = $1, content = $2 WHERE id = $3", [@title, @content, @id])
+  def update(conn)
+    conn.exec("UPDATE memos SET title = $1, content = $2 WHERE id = $3", [@title, @content, @id])
   end
 
-  def self.delete(id)
-    connection = PG.connect(dbname: 'memo')
-    connection.exec('DELETE FROM memos WHERE id = $1',[id]).first
+  def self.delete(conn, id)
+    conn.exec('DELETE FROM memos WHERE id = $1',[id]).first
   end
 
-  def self.show_memo(id)
-    connection = PG.connect(dbname: 'memo')
-    memo = connection.exec('SELECT * FROM memos WHERE id = $1',[id]).first
+  def self.show_memo(conn, id)
+    memo = conn.exec('SELECT * FROM memos WHERE id = $1',[id]).first
     Memo.new(memo['id'], memo['title'], memo['content'])
   end
 end
--- memo_app.rb
+++ memo_app.rb
@@ -7,10 +7,15 @@ require_relative 'memo'
 
 set :environment, :development
 
+def connect_db
+  PG.connect(dbname: 'memo')
+end
+conn = connect_db
+
 # rubocop:disable Metrics/BlockLength
 namespace '/memos' do
   get '' do
-    @memos = Memo.index
+    @memos = Memo.index(conn)
     erb :index
   end
 
@@ -19,27 +24,27 @@ namespace '/memos' do
   end
 
   post '' do
-    Memo.new(nil, params[:title], params[:content]).create
+    Memo.new(nil, params[:title], params[:content]).create(conn)
     redirect '/memos'
   end
 
   get '/:id' do
-    @memo = Memo.show_memo(params[:id])
+    @memo = Memo.show_memo(conn, params[:id])
     erb :show
   end
 
   get '/:id/edit' do
-    @memo = Memo.show_memo(params[:id])
+    @memo = Memo.show_memo(conn, params[:id])
     erb :edit
   end
 
   patch '' do
-    @memo = Memo.new(params[:id], params[:title], params[:content]).update
+    @memo = Memo.new(params[:id], params[:title], params[:content]).update(conn)
     redirect '/memos'
   end
 
   delete '/:id' do
-    Memo.delete(params[:id])
+    Memo.delete(conn, params[:id])
     redirect '/memos'
   end
 end
