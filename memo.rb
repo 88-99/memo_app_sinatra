77fbe2a96d25de6797200f38dc4375e041b14ac7
88-99 <xz835.525o39@yahoo.com>
2023-01-04 15:28:48 +0900

    exec -> exec_paramsに変更

--- memo.rb
+++ memo.rb
@@ -19,19 +19,19 @@ class Memo
   end
 
   def create(conn)
-    conn.exec('INSERT INTO memos VALUES ($1, $2, $3)', [@id, @title, @content])
+    conn.exec_params('INSERT INTO memos VALUES ($1, $2, $3)', [@id, @title, @content])
   end
 
   def update(conn)
-    conn.exec('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [@title, @content, @id])
+    conn.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [@title, @content, @id])
   end
 
   def self.delete(conn, id)
-    conn.exec('DELETE FROM memos WHERE id = $1', [id]).first
+    conn.exec_params('DELETE FROM memos WHERE id = $1', [id]).first
   end
 
   def self.show_memo(conn, id)
-    memo = conn.exec('SELECT * FROM memos WHERE id = $1', [id]).first
+    memo = conn.exec_params('SELECT * FROM memos WHERE id = $1', [id]).first
     Memo.new(memo['id'], memo['title'], memo['content'])
   end
 end
